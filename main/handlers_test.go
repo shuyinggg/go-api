30c085c1231280d838ed63573b82fb316f2ae8d2
shuyinggg <shuying3@uw.edu>
2019-01-22 10:45:25 -0800

    fix tests and style

--- main/handlers_test.go
+++ main/handlers_test.go
@@ -26,7 +26,8 @@ func TestGetCookiesNull(t *testing.T) {
 }
 
 func TestPostCookies(t *testing.T) {
-	cookies := []map[string]string{
+	cookiesString := `[{"Name":"Cookie1","Value":"This is cookie1"},{"Name":"Cookie2","Value":"This is cookie2"}]`
+	expected := []map[string]string{
 		{
 			"Name":  "Cookie1",
 			"Value": "This is cookie1",
@@ -36,16 +37,17 @@ func TestPostCookies(t *testing.T) {
 			"Value": "This is cookie2",
 		},
 	}
+	
 	// convert that into json
 	//test POST:
-	req, _ := http.NewRequest("POST", url, strings.NewReader(cookies))
+	req, _ := http.NewRequest("POST", url, strings.NewReader(cookiesString))
 	res := httptest.NewRecorder()
 	Methods(res, req)
 	body, _ := ioutil.ReadAll(res.Body)
-	t.Log(body)
+	//t.Log(body)
 	var actual []byte
 	json.Unmarshal(body, &actual)
-	assertEqual(t, body, cookies, 1)
+	assertEqual(t, body, expected)
 }
 
 func TestDeleteCookies(t *testing.T) {
@@ -61,9 +63,13 @@ func TestDeleteCookies(t *testing.T) {
 
 func TestPostDuplicateCookies(t *testing.T) {
 	cookies := `[{"Name":"Cookie1","Value":"This is cookie1"},{"Name":"Cookie1","Value":"This is cookie2"}]`
-	var sample []map[string]string
-	sample1 := map[string]string{"err": "true", "msg": "Duplicate! Please check input"}
-	sample = append(sample, sample1)
+	sample := []map[string]string{
+		{
+			"err": "true", 
+			"msg": "Duplicate! Please check input",
+		},
+	}
+	
 	//test POST:
 	req, _ := http.NewRequest("POST", url, strings.NewReader(cookies))
 	res := httptest.NewRecorder()
@@ -71,14 +77,17 @@ func TestPostDuplicateCookies(t *testing.T) {
 	body, _ := ioutil.ReadAll(res.Body)
 	var actual []byte
 	json.Unmarshal(body, &actual)
-	assertEqual(t, body, sample, 2)
+	assertErrorMsg(t, body, sample)
 }
 
 func TestPostNull(t *testing.T) {
 	v := ""
-	var sample []map[string]string
-	sample1 := map[string]string{"err": "true", "msg": "Invalid input! Please check input"}
-	sample = append(sample, sample1)
+	sample := []map[string]string{
+		{
+			"err": "true", 
+			"msg": "Invalid input! Please check input",
+		},
+	}
 	//test POST:
 	req, _ := http.NewRequest("POST", url, strings.NewReader(v))
 	res := httptest.NewRecorder()
@@ -86,46 +95,54 @@ func TestPostNull(t *testing.T) {
 	body, _ := ioutil.ReadAll(res.Body)
 	var actual []byte
 	json.Unmarshal(body, &actual)
-	assertEqual(t, body, sample, 2)
+	assertErrorMsg(t, body, sample)
 }
 
 func assertEqualNil(t *testing.T, actualRes []byte) {
 	if !bytes.Equal(actualRes, nil) {
-		t.Errorf("Fail, got %s, expected %s", string(actualRes), `[]`)
+		t.Errorf("FAIL, got %s, expected %s", string(actualRes), `[]`)
 	} else {
 		t.Logf("PASS, got %s, expected %s", string(actualRes), `[]`)
 	}
 }
 
-func assertEqual(t *testing.T, actualRes []byte, predictedRes []map[string]string, ty int) {
+func assertEqual(t *testing.T, actualRes []byte, predictedRes []map[string]string) {
+	actualSs := string(actualRes[:]) //slice then toString
+	actualS := strings.Split(actualSs, "},{")
+	//fmt.Println(actualS)
+	var pairs []map[string]string
+	for ind := range actualS {
+			result := strings.FieldsFunc(actualS[ind], split)
+			length := len(result[3])
+			pairs = append(pairs, map[string]string{"err": result[1], "msg": result[3][0 : length-2]})
+	}
+	equal := Equal(pairs, predictedRes)
+
+	if !equal {
+		t.Errorf("Fail, got %v, expected %v", pairs, predictedRes)
+	}
+}
+
+func assertErrorMsg(t *testing.T, actualRes []byte, predictedRes []map[string]string) {
 	actualSs := string(actualRes[:]) //slice then toString
 	actualS := strings.Split(actualSs, "},{")
 	//fmt.Println(actualS)
 	var pairs []map[string]string
 	for ind := range actualS {
 		result := strings.FieldsFunc(actualS[ind], split)
-		if ty == 1 { //cookies
-			pairs = append(pairs, map[string]string{"Name": result[1], "Value": result[3]})
-		} else { //errors
-			length := len(result[3])
-			pairs = append(pairs, map[string]string{"err": result[1], "msg": result[3][0 : length-2]})
-		}
-
+		pairs = append(pairs, map[string]string{"Name": result[1], "Value": result[3]})
 	}
+}
+
+func Equal(pairs []map[string]string, predictedRes []map[string]string) bool {
 	equal := true
 	for ind := range pairs {
 		if reflect.DeepEqual(pairs[ind], predictedRes[ind]) {
 			equal = false
 		}
 	}
-	if equal {
-		t.Logf("PASS, got %v, expected %v", pairs, predictedRes)
-	} else {
-		t.Errorf("Fail, got %v, expected %v", pairs, predictedRes)
-	}
-
+	return equal
 }
-
 func split(r rune) bool {
 	return r == ':' || r == ','
 }
